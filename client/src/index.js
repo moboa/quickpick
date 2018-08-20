281aecddfa188586456d6c5960888326279f1e99
moboa <mbajifope@gmail.com>
2018-08-19 22:02:27 -0400

    Connect client to server with socket.io

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
-import { List, Map } from 'immutable';
+import io from 'socket.io-client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
@@ -10,14 +10,13 @@ import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducer';
 
 const store = createStore(reducer);
-store.dispatch({
-    type: 'SET_STATE',
-    state: {
-        vote: {
-            pair: ['Phoenix Wright', 'Shadow Of The Colossus'],
-            tally: {'Phoenix Wright': 1}
-        }
-    }
+
+const socket = io(`${window.location.protocol}//${window.location.hostname}:7001`);
+socket.on('state', state => {
+    store.dispatch({
+        type: 'SET_STATE', 
+        state: state
+    });
 });
 
 ReactDOM.render(
